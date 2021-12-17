351197ee64f34a5dd50e1a4f345ed9782577054b
Joshua Hamilton <joshua@tinydog.io>
2021-12-16 21:28:05 -0500

    Updates

--- _next/static/0uCmVk9wK8hSABDfYC3sE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-ebce61c517522efb.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],"/privacy-policy":["static/chunks/pages/privacy-policy-7af72806e217b2d9.js"],sortedPages:["/","/_app","/_error","/privacy-policy"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
